8339354a31e051d84902ceb46889452cf9059364
Toby Flynn <toby_flynn@btinternet.com>
2020-02-13 00:09:47 +0000

    Edits to val script

--- apps/adi/tools/compare_cpu_mpi.py
+++ apps/adi/tools/compare_cpu_mpi.py
@@ -2,10 +2,13 @@
 
 import numpy as np
 
-mpi_array = np.genfromtxt('../build/mpi.csv', delimiter=' ')
-cpu_array = np.genfromtxt('../build/cpu.csv', delimiter=' ')
+mpi_array = np.genfromtxt('../build/mpi-n128-i1.csv', delimiter=' ')
+cpu_array = np.genfromtxt('../build/cpu-n128-i1.csv', delimiter=' ')
 
 difference = np.abs(mpi_array - cpu_array)
 
 print("Max difference: " + str(np.max(difference)))
+print("Min difference: " + str(np.min(difference)))
+print("Median difference: " + str(np.median(difference)))
 print("Sum difference: " + str(np.sum(difference)))
+print("Avg difference: " + str(np.sum(difference) / difference.size))
